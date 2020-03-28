34feb680d0aef53590dc67d8948b3df3c727aeae
bengroneman <bluegroneman10@gmail.com>
2020-03-28 11:29:22 -0600

    chapter 6 finished

---
+++ PreCourseWork/Chapter 6 - Hashes/anagram.rb
@@ -0,0 +1,24 @@
+# Expected output
+# ["demo", "dome", "mode"]
+#["neon", "none"]
+words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
+          'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+          'flow', 'neon']
+
+result = {}
+
+words.each do |word|
+  key = word.split('').sort.join
+  if result.has_key?(key)
+    result[key].push(word)
+  else
+    result[key] = [word]
+  end
+end
+
+p result
+
+result.each_value do |v|
+  puts "_______"
+  p v
+end

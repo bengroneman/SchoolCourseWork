d27da75118dff4d33bb14c4873df654a42224316
bengroneman <bluegroneman10@gmail.com>
2020-03-31 13:51:44 -0600

    catching up

--- PreCourseWork/exercises.rb
+++ PreCourseWork/exercises.rb
@@ -73,14 +73,66 @@ contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
 
 contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
 
-contacts.each do |contact|
-  contact[:email] = contact_data.first
-  contact_data.shift!
-  contact[:address] = contact_data.first
-  contact_data.shift!
-  contact[:phone_number] = contact_data.first
-  contact_data.shift!
+# It is important to note that method definitions have isolated variable scopes
+# You cannot access variables that outside the scope of the method
+# However, running methods on objects allows you to access the first outer scope
+
+numbers = (1..100).to_a
+
+flow_control = true
+counter = 1
+
+while flow_control
+  choice = Random.new()
+  i = choice.rand(1..100)
+  p numbers[i - 1]
+  counter += 1
+  flow_control = false if counter > 5
 end
-p contacts
 
+count = 1 
 
+until count == 10
+  puts count
+  count += 1
+end
+
+numbers = [7, 9, 13, 25, 18]
+
+counter = 0
+p "test"
+until counter == numbers.size do
+  puts numbers[counter]
+  counter += 1
+end
+
+for i in 1..100
+  puts i if i.odd?
+end 
+
+friends = ['Sarah', 'John', 'Hannah', 'Dave']
+
+friends.each { |friend| p "Hello, #{friend}" }
+
+count = 1
+
+numbers = (1..5).to_a
+loop do
+  p "even: #{numbers[count]}" if numbers[count].even?
+  p "odd: #{numbers[count]}" if numbers[count].odd?
+  count += 1
+  break if count >= 5
+end
+
+loop do
+  number = rand(100)
+  puts number
+  break if number <= 10 && number >= 0
+  # or break if number.between?(0, 10)
+end
+
+process_the_loop = [true, false].sample
+
+loop do
+  
+end

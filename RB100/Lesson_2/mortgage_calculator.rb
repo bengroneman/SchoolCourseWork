a5829224d1dd42fce8ad1bbab63d9f13c604d825
bengroneman <bluegroneman10@gmail.com>
2020-07-06 10:09:08 -0600

    rubocop refactor

--- RB100/Lesson_2/mortgage_calculator.rb
+++ RB100/Lesson_2/mortgage_calculator.rb
@@ -1,41 +1,17 @@
-require "pry"
-
 require_relative "utils"
 include Utils
 
 def get_loan_info
-  # TODO: maybe we can use procs and blocks to persist data
-  # WIth the goal being to remember the previous entry if the user fails to input a good APR, that way they won't have to enter the loan amount as well as the apr again.
   loan_info = {}
-  loop do
-    amount = prompt_user("How much money will you need for your loan (e.g. 100000)?")
-    unless !amount.empty? && valid_number?(amount)
-      prompt("Whoops, that loan amount isn't valid, try again")
-      next
-    end
-    loan_info[:amount] = amount.to_f
-    break
-  end
 
-  loop do
-    loan_apr = prompt_user("What APR are you getting (e.g. 4.02)?")
-    unless !loan_apr.empty?
-      prompt("Whoops, that APR isn't valid, try again")
-      next
-    end
-    raw_apr = format_percent(loan_apr)
-    if raw_apr
-      loan_info[:monthly_interest_rate] = get_monthly_interest_rate(raw_apr.to_f)
-    else
-      puts "Your APR was not formatted correctly. Try something like this 5.02 or %5.02"
-      next
-    end
-    break
-  end
+  loan_info[:amount] = get_loan_amount()
+  raw_apr = get_apr()
+  loan_info[:monthly_interest_rate] = get_monthly_interest_rate(raw_apr.to_f)
 
-  # TODO: allow the user to input a syntax similar to this (3y 18m) and parse it.
-  loan_info[:duration] = prompt_user("How long will you have this loan in months (e.g. 48)?").to_f
-  return loan_info
+  # TODO: allow the user to input a syntax similar to this (3y 18m) and parse
+  loan_info[:duration] = ask("How long will you have this loan in months \
+                              (e.g. 48)?").to_f
+  loan_info
 end
 
 def mortgage_calculator
@@ -44,15 +20,13 @@ def mortgage_calculator
   monthly_payment = get_monthly_payment(loan_info)
 
   puts "Your total monthly payment is $#{monthly_payment}"
-  answer = prompt_user("Do you want to calculate again (type y or n)?")
+  answer = ask("Do you want to calculate again (type y or n)?")
   if answer.downcase.eql?("y")
-    puts "Goodbye!"
-  else
     puts "Alrighty we are spinning up again!"
     mortgage_calculator()
+  else
+    puts "Goodbye!"
   end
 end
 
 mortgage_calculator()
-
-__END__
--- RB100/Lesson_2/utils.rb
+++ RB100/Lesson_2/utils.rb
@@ -1,32 +1,57 @@
-module Utils 
+module Utils
   def prompt(msg)
     puts "=| #{msg}"
   end
 
-  def prompt_user(question)
+  def get_loan_amount
+    loop do
+      amount = ask("How much money will you need for your loan (e.g. 100000)?")
+      unless !amount.empty? && valid_number?(amount)
+        prompt("Whoops, that loan amount isn't valid, try again")
+        next
+      end
+      return amount
+    end
+  end
+
+  def get_apr
+    loop do
+      loan_apr = ask("What APR are you getting (e.g. 4.02)?")
+      unless !loan_apr.empty?
+        prompt("Whoops, that APR isn't valid, try again")
+        next
+      end
+      raw_apr = format_percent(loan_apr)
+      return raw_apr if raw_apr
+      puts "Your APR was not formatted correctly. \
+            Try something like this 5.02 or 5.02 %"
+      next
+    end
+  end
+
+  def ask(question)
     # Return user input
     puts question
-
     gets.chomp
   end
 
   def format_percent(percent)
     # Find percent or %
-    regex = /[+-]?(?=[^+-\/*^])\d+(\.\d+)*/
+    regex = %r(/[+-]?(?=[^+-\/*^])\d+(\.\d+)*/)
     is_match = regex.match(percent)
     if is_match
       is_match[0]
     else
       false
     end
-    #if !regex.nil? && valid_number?(percent)
-    #else
   end
 
   def get_monthly_payment(loan_config)
     numerator = loan_config[:monthly_interest_rate]
-    denominator = (1 - (1 + loan_config[:monthly_interest_rate])**(-loan_config[:duration]))
-    loan_config[:amount] * (numerator / denominator) 
+    denominator = (1 - \
+                  (1 + loan_config[:monthly_interest_rate])\
+                    **(-loan_config[:duration]))
+    loan_config[:amount] * (numerator / denominator)
   end
 
   def valid_number?(num)
